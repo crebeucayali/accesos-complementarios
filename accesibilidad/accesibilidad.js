d83b24c03233fda8a7cfd9e66715ffc1940e4c8c
crebeucayali <senordelosmilagroscrebe@gmail.com>
2026-07-10 10:30:53 -0500

    contenido de la página

---
+++ accesibilidad/accesibilidad.js
@@ -0,0 +1,6 @@
+(function(){
+  var script = document.createElement("script");
+  script.src = "../contenido/accesibilidad/accesibilidad.js";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
